3b22a2dbfeb40f80a990f080ed4e5f4162723413
1751012051phong <55686484+1751012051phong@users.noreply.github.com>
2020-08-04 11:10:35 +0700

    feat

--- python-flask/library/app.py
+++ python-flask/library/app.py
@@ -4,8 +4,8 @@ from flask import Flask, render_template
 
 @app.route("/")
 def index():
-   return "hello world"
+    return "hello world"
 
 
 if __name__ == "__main__":
-   app.run(debug=True)
+    app.run(debug=True)
