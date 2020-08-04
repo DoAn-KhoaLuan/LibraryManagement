30a75d602fe5f429084a6ca661ebe5f4632833d8
1751012051phong <55686484+1751012051phong@users.noreply.github.com>
2020-08-04 16:49:31 +0700

    feat(BE):update

---
+++ python-flask/library/DAL/models/Authors.py
@@ -0,0 +1,6 @@
+from library.app import db
+
+class Authors(db.Model):
+    author_id
+    author_name
+    
---
+++ python-flask/library/DAL/models/Categories.py
@@ -0,0 +1,7 @@
+from library.app import db
+
+class Categories(db.Model):
+    category_id
+    category_name
+    description
+    note
