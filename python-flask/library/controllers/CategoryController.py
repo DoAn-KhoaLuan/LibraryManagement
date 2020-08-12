eab40fa7ad38ff5a0cb18482b07067720513cc1e
phong <1751012051phong@ou.edu.vn>
2020-08-12 12:21:10 +0700

    feat(BE) update

--- python-flask/library/controllers/CategoryController.py
+++ python-flask/library/controllers/CategoryController.py
@@ -9,24 +9,24 @@ import json
 from library.Common.Rsp.SingleRsp import ErrorRsp
 
 
-
 @app.route('/admin/category-management/get-categories', methods=['GET', 'POST'])
 def GetCategories():
-        req = GetItemsByPageReq(request.json)
-        result = CategorySvc.GetCategoriesByPage(req)
-        res = GetItemsByPageRsp(has_next=result['has_next'], has_prev=result['has_prev'], items=result['categories']).serialize()
-        return jsonify(res)
+    req = GetItemsByPageReq(request.json)
+    result = CategorySvc.GetCategoriesByPage(req)
+    res = GetItemsByPageRsp(has_next=result['has_next'], has_prev=result['has_prev'],
+                            items=result['categories']).serialize()
+    return jsonify(res)
+
 
 @app.route('/admin/category-management/create-category', methods=['POST'])
 def CreateCategory():
-        req = CreateCategoryReq(request.json)
-        created_category = CategorySvc.CreateCategory(req)
-        return jsonify(created_category)
+    req = CreateCategoryReq(request.json)
+    created_category = CategorySvc.CreateCategory(req)
+    return jsonify(created_category)
+
 
 @app.route('/admin/category-management/update-category', methods=['POST'])
 def UpdateCategory():
-        req = UpdateCategoryReq(request.json)
-        updated_category = CategorySvc.UpdateCategory(req)
-        print(updated_category)
-        return jsonify(updated_category)
-
+    req = UpdateCategoryReq(request.json)
+    updated_category = CategorySvc.UpdateCategory(req)
+    return jsonify(updated_category)
