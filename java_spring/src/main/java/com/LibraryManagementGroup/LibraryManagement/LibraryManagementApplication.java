a258f382c590e5296c972652caff7ad28f0ce5ee
kimhai <shinichi24567@gmail.com>
2021-01-10 23:35:28 +0700

    feat(BE): init project java spring

---
+++ java_spring/src/main/java/com/LibraryManagementGroup/LibraryManagement/LibraryManagementApplication.java
@@ -0,0 +1,15 @@
+package com.LibraryManagementGroup.LibraryManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+public class LibraryManagementApplication {
+
+	public static void main(String[] args) {
+		System.out.println(("HEELLLO WORLD!! "));
+		SpringApplication.run(LibraryManagementApplication.class, args);
+	}
+
+}
